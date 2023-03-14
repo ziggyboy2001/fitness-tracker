6d8b31257de242f78264b08a001f91fba2c2c44b
TheHighnote <ryanhighnote7@gmail.com>
2023-03-14 10:22:19 -0500

    started working on routines

--- db/routines.js
+++ db/routines.js
@@ -1,7 +1,22 @@
 const client = require("./client");
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {}
-
+//   try {
+//   const {
+//     rows: [user],
+//   } = await client.query(
+//     `
+//       INSERT INTO routines("creatorId", "isPublic", name, goal) 
+//       VALUES($1, $2, $3, $4) 
+//       RETURNING *;
+//     `,
+//     [creatorId, isPublic, name, goal]
+//   );
+//   return user;
+// } catch (error) {
+//   console.log(error);
+//   throw error;
+// }
 async function getRoutineById(id) {}
 
 async function getRoutinesWithoutActivities() {}
