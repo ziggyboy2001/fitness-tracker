97969389ec1a624e4b4900cc51d2979531fadecd
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-16 14:29:01 -0500

    working on GET / axctivities

--- api/activities.js
+++ api/activities.js
@@ -1,8 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const { getAllActivities } = require("../db");
+const { requireUser } = require("./utils");
 // GET /api/activities/:activityId/routines
+router.get("/", async (req, res, next) => {
+  try {
+    const allActivities = await getAllActivities();
+    const activities = allActivities.filter((activity) => {
+      if (activity.id) {
+        return activity.id || (req.user && activity.id === req.user.id);
+      }
+      console.log(activity.id, "1234ACTIVITIES");
+    });
 
+    res.send({
+      activities,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 // GET /api/activities
 
 // POST /api/activities
