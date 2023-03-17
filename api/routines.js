98281706ba84e170f408d1793f0cd8c5f6fae0cf
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-17 12:50:26 -0500

    beginning routine routes

--- api/routines.js
+++ api/routines.js
@@ -1,10 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { createRoutine } = require("../db");
+const { requireUser } = require("./utils");
 
 // GET /api/routines
 
 // POST /api/routines
+router.post("/", requireUser, async (req, res, next) => {
+  const { name, description = "" } = req.body;
 
+  const postData = {
+    authorId: req.user.id,
+    name,
+    description,
+  };
+
+  try {
+    const post = await createRoutine(postData);
+    if (post) {
+      res.send(post);
+    } else {
+      next({ name: "PostCreationError", message: "Error creating post." });
+    }
+  } catch ({ name, message }) {
+    next({
+      name,
+      message: `An activity with name ${postData.name} already exists`,
+    });
+  }
+});
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
